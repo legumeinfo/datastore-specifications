25373439287f4e5c4bfbc609a9e97cb3446461a1
adf-ncgr <adf@ncgr.org>
2022-04-06 16:21:49 -0600

    properly handle updating Parent references when using clobbering time mode.

--- scripts/add_IDs_to_gff_features.pl
+++ scripts/add_IDs_to_gff_features.pl
@@ -6,6 +6,8 @@ GetOptions(
     "clobber_type=s" => \@clobber_types,
 );
 my %clobber_types = map {$_ => 1;} @clobber_types;
+#need to remember IDs we've clobbered in case they are used as Parent references
+my %clobbered_map;
 #we'll use a strategy where IDs are based on parent IDs + counts for uniqueness
 my %children_count;
 while (<>) {
@@ -19,11 +21,18 @@ while (<>) {
     my @attrs = split /;/, $attrs;
     my %attrs = map {my ($key, $value) = /^([^=]+)=(.*)/; $key => $value;} @attrs;
     if ($attrs{ID} && ! $clobber_types{$data[2]}) {
+	my $parent = $attrs{Parent};
+	if (defined $clobbered_map{$parent}) {
+		s/\bParent=$parent/Parent=$clobbered_map{$parent}/;
+	}
         print;
         print "\n";
     }
     else {
         my $parent = $attrs{Parent};
+	if (defined $clobbered_map{$parent}) {
+		s/\bParent=$parent/Parent=$clobbered_map{$parent}/;
+	}
         my $count_by_type = $children_count{$parent};
         if (!defined $count_by_type) {
             $count_by_type = {};
@@ -31,10 +40,13 @@ while (<>) {
         }
         my $type = $data[2];
         my $count = ++$count_by_type->{$type};
+	my $new_id = "$parent-$type-$count";
         if ($attrs{ID}) {
-            $attrs =~ s/\bID=[^;]*;?//;
+            $attrs =~ s/\bID=([^;]*);?//;
+	    print STDERR "$1\t$new_id\n";
+	    $clobbered_map{$1} = $new_id;
         }
-        $attrs = "ID=$parent-$type-$count;".$attrs;
+        $attrs = "ID=$new_id;".$attrs;
         $data[8] = $attrs;
         print join("\t", @data), "\n";
     }
