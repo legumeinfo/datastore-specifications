be1bd582012737378c0e7156824a127a0c7d7dee
cann0010 <scannon@iastate.edu>
2022-10-12 12:01:32 -0500

    Add section to process genome assembly files. Write MANIFEST files.

--- scripts/prep_genomic_collection.pl
+++ scripts/prep_genomic_collection.pl
@@ -4,9 +4,8 @@ use warnings;
 use strict;
 use YAML::Tiny 'LoadFile';
 use Getopt::Long;
-use Data::Dumper;
-use IO::Zlib;
 use File::Copy;
+use File::Basename;
 use feature "say";
 
 my $usage = <<EOS;
@@ -53,7 +52,6 @@ if ($gene_hash){
 my $yaml = YAML::Tiny->read( $config );
 
 my $confobj = LoadFile($config);
-#print Dumper($confobj);
 
 my %coll_hsh;
 for (keys %{$confobj->{collection_info}}){ $coll_hsh{$_} = $confobj->{collection_info}->{$_} }
@@ -67,19 +65,46 @@ for (keys %{$confobj->{prefixes}}){ $prefix_hsh{$_} = $confobj->{prefixes}->{$_}
 my %readme_hsh;
 for (keys %{$confobj->{readme_info}}){ $readme_hsh{$_} = $confobj->{readme_info}->{$_} }
 
+# Make some variables for prefixes, for convenience
 my $GNMCOL = "$coll_hsh{accession}.$coll_hsh{gnm_ver}.$coll_hsh{genome_key}";
 my $ANNCOL = "$coll_hsh{accession}.$coll_hsh{gnm_ver}.$coll_hsh{ann_ver}.$coll_hsh{annot_key}";
 my $GENSP = $coll_hsh{gensp};
-my $TO_GNM_PREFIX = "$coll_hsh{gensp}.$coll_hsh{accession}.$coll_hsh{gnm_ver}";
-my $TO_ANN_PREFIX = "$coll_hsh{gensp}.$coll_hsh{accession}.$coll_hsh{gnm_ver}.$coll_hsh{ann_ver}";
+my $TO_GNM_PREFIX = "$GENSP.$coll_hsh{accession}.$coll_hsh{gnm_ver}";
+my $TO_ANN_PREFIX = "$GENSP.$coll_hsh{accession}.$coll_hsh{gnm_ver}.$coll_hsh{ann_ver}";
 
 # Make collection directories
 my $WD = "$dir_hsh{work_dir}";
 my $ANNDIR = "$WD/annotations/$ANNCOL";
-unless (-d $ANNDIR) {mkdir -p $ANNDIR or die "Can't make directory $ANNDIR: $!\n"}
-my $GNMDIR = "$WD/annotations/$ANNCOL";
-unless (-d $GNMDIR) {mkdir -p $GNMDIR or die "Can't make directory $GNMDIR: $!\n"}
+unless (-d "$WD/annotations") {mkdir "$WD/annotations" or die "Can't make directory $WD/annotations: $!\n"}
+unless (-d $ANNDIR) {mkdir $ANNDIR or die "Can't make directory $ANNDIR: $!\n"}
+my $GNMDIR = "$WD/genomes/$GNMCOL";
+unless (-d "$WD/genomes") {mkdir "$WD/genomes" or die "Can't make directory $WD/genomes: $!\n"}
+unless (-d $GNMDIR) {mkdir $GNMDIR or die "Can't make directory $GNMDIR: $!\n"}
 
+# Make metadata files
+my $ANN_MAN_CORR = "$ANNDIR/MANIFEST.$ANNCOL.correspondence.yml";
+my $ANN_MAN_DESCR = "$ANNDIR/MANIFEST.$ANNCOL.descriptions.yml";
+my $ANN_README = "$ANNDIR/README.$ANNCOL.yml";
+my $GNM_MAN_CORR = "$GNMDIR/MANIFEST.$GNMCOL.correspondence.yml";
+my $GNM_MAN_DESCR = "$GNMDIR/MANIFEST.$GNMCOL.descriptions.yml";
+my $GNM_README = "$GNMDIR/README.$GNMCOL.yml";
+# Remove existing metadata files, then open them for appending
+for my $file ($ANN_MAN_CORR, $ANN_MAN_DESCR, $ANN_README, 
+              $GNM_MAN_CORR, $GNM_MAN_DESCR, $GNM_README){
+  if (-e $file){ unlink $file or die "Can't unlink metadata file $file: $!" }
+}
+open(my $ANN_MAN_CORR_FH, '>>', $ANN_MAN_CORR) or die "Can't open out $ANN_MAN_CORR: $!";
+open(my $ANN_MAN_DESCR_FH, '>>', $ANN_MAN_DESCR) or die "Can't open out $ANN_MAN_DESCR: $!";
+open(my $ANN_README_FH, '>>', $ANN_README) or die "Can't open out $ANN_README: $!";
+open(my $GNM_MAN_CORR_FH, '>>', $GNM_MAN_CORR) or die "Can't open out $GNM_MAN_CORR: $!";
+open(my $GNM_MAN_DESCR_FH, '>>', $GNM_MAN_DESCR) or die "Can't open out $GNM_MAN_DESCR: $!";
+open(my $GNM_README_FH, '>>', $GNM_README) or die "Can't open out $GNM_README: $!";
+print $ANN_MAN_CORR_FH "---\n# filename in this repository: previous names\n";
+print $GNM_MAN_CORR_FH "---\n# filename in this repository: previous names\n";
+print $ANN_README_FH "---\n";
+print $ANN_MAN_DESCR_FH "---\n# filename in this repository: description\n";
+print $GNM_MAN_DESCR_FH "---\n# filename in this repository: description\n";
+print $GNM_README_FH "---\n";
 
 ##################################################
 say "\n== Copying over \"as-is\" annotation information files, if present, unchanged ==";
@@ -89,10 +114,13 @@ for my $fr_to_hsh (@{$confobj->{from_to_as_is}}){
   say "Copying from-to as-is annotation files:";
   say "  $AS_IS_FROM_FILE"; 
   say "  $ANNDIR/$GENSP.$ANNCOL.$fr_to_hsh->{to}\n";
+  my $to_name_base = basename($AS_IS_TO_FILE);
+  my $from_name_base = basename($AS_IS_FROM_FILE);
+  print $ANN_MAN_CORR_FH "$to_name_base.gz: $from_name_base\n";
+  print $ANN_MAN_DESCR_FH "$to_name_base.gz: $fr_to_hsh->{description}\n";
   if ($AS_IS_FROM_FILE =~ /gz$/){
-    my $AS_IS_FROM_FH = new IO::Zlib;
-    $AS_IS_FROM_FH->open($AS_IS_FROM_FILE, "rb");
-    open (my $AS_IS_TO_FH, ">", $AS_IS_TO_FILE) or die "Can't open out $AS_IS_TO_FILE: $!\n";
+    open(my $AS_IS_FROM_FH, "gzcat $AS_IS_FROM_FILE |") or die "Can't do gunzip $AS_IS_FROM_FILE|: $!";
+    open(my $AS_IS_TO_FH, ">", $AS_IS_TO_FILE) or die "Can't open out $AS_IS_TO_FILE: $!\n";
     while (<$AS_IS_FROM_FH>) {
       print $AS_IS_TO_FH $_;
     }
@@ -111,7 +139,7 @@ my $GENOME_FILE_START;
 for my $fr_to_hsh (@{$confobj->{from_to_genome}}){ 
   if ($fr_to_hsh->{to} =~ /genome_main/){
     $GENOME_FILE_START = 
-      "$dir_hsh{work_dir}/$dir_hsh{from_genome_dir}/$prefix_hsh{from_genome_prefix}.$fr_to_hsh->{from}";
+      "$WD/$dir_hsh{from_genome_dir}/$prefix_hsh{from_genome_prefix}.$fr_to_hsh->{from}";
   }
 }
 say $GENOME_FILE_START;
@@ -128,8 +156,7 @@ else {
   $CHR_HASH = "$WD/genomes/$GNMCOL/$GENSP.$GNMCOL.chr_name_hash.tsv";
   open (my $CHR_HASH_FH, ">", $CHR_HASH) or die "Can't open out $CHR_HASH: $!\n";
   say "Generating hash of old/new chromosome & scaffold IDs.";
-  my $GNM_MAIN_FH = new IO::Zlib;
-  $GNM_MAIN_FH->open($GENOME_FILE_START, "rb");
+  open(my $GNM_MAIN_FH, "gzcat $GENOME_FILE_START|") or die "Can't do gzcat $GENOME_FILE_START|: $!";
   while (my $line = <$GNM_MAIN_FH>){
     if ($line =~ /^>(\S+)/){
       my $chr = $1;
@@ -138,6 +165,9 @@ else {
     }
   }
 }
+my $to_name_base = basename($CHR_HASH);
+print $ANN_MAN_CORR_FH "$to_name_base.gz: No prior file\n";
+print $ANN_MAN_DESCR_FH "$to_name_base.gz: Hash file of old/new chromosome and scaffold IDs\n";
 
 ##################################################
 say "\n== Making a hash of old/new gene IDs, to go to the annotations directory ==";
@@ -151,7 +181,7 @@ for my $fr_to_hsh (@{$confobj->{from_to_cds_mrna}}){
     $CDS_FILE_START = 
       "$dir_hsh{work_dir}/$dir_hsh{from_annot_dir}/$prefix_hsh{from_annot_prefix}.$fr_to_hsh->{from}";
     $SPLICE_RX = qr/$fr_to_hsh->{splice}/;
-    say $SPLICE_RX;
+    say "  Splice regex: $SPLICE_RX";
   }
 }
 say $CDS_FILE_START;
@@ -166,8 +196,7 @@ if ($gene_hash){
 }
 else {
   say "Generating hash of old/new gene IDs.";
-  my $CDS_IN_FH = new IO::Zlib;
-  $CDS_IN_FH->open($CDS_FILE_START, "rb");
+  open(my $CDS_IN_FH, "gzcat $CDS_FILE_START |") or die "Can't do gzcat $CDS_FILE_START|: $!";
   $GENE_HASH = "$WD/annotations/$ANNCOL/$GENSP.$ANNCOL.gene_name_hash.tsv";
   open (my $GENE_HASH_FH, ">", $GENE_HASH) or die "Can't open out $GENE_HASH: $!\n";
   my %seen_gene_id;
@@ -182,15 +211,22 @@ else {
     }
   }
 }
+$to_name_base = basename($GENE_HASH);
+print $ANN_MAN_CORR_FH "$to_name_base.gz: No prior file\n";
+print $ANN_MAN_DESCR_FH "$to_name_base.gz: Hash file of old/new gene IDs\n";
 
 ##################################################
-say "\n== Processing the nucleotide sequence files ==";
+say "\n== Processing the gene nucleotide files (CDS, mRNA) ==";
 for my $fr_to_hsh (@{$confobj->{from_to_cds_mrna}}){ 
   my $SEQ_FROM_FILE = "$WD/$dir_hsh{from_annot_dir}/$prefix_hsh{from_annot_prefix}.$fr_to_hsh->{from}"; 
   my $SEQ_TO_FILE = "$ANNDIR/$GENSP.$ANNCOL.$fr_to_hsh->{to}";
   say "Converting from to:";
   say "  $SEQ_FROM_FILE";
   say "  $SEQ_TO_FILE";
+  my $to_name_base = basename($SEQ_TO_FILE);
+  my $from_name_base = basename($SEQ_FROM_FILE);
+  print $ANN_MAN_CORR_FH "$to_name_base.gz: $from_name_base\n";
+  print $ANN_MAN_DESCR_FH "$to_name_base.gz: $fr_to_hsh->{description}\n";
   my $SPLICE_RX = $fr_to_hsh->{splice};
   $SPLICE_RX =~ s/\"//g; # Strip surrounding quotes if any. We'll add them below.
   say "  SPLICE REGEX: \"$SPLICE_RX\"";
@@ -207,6 +243,10 @@ for my $fr_to_hsh (@{$confobj->{from_to_protein}}){
   say "Converting from to:";
   say "  $SEQ_FROM_FILE";
   say "  $SEQ_TO_FILE";
+  my $to_name_base = basename($SEQ_TO_FILE);
+  my $from_name_base = basename($SEQ_FROM_FILE);
+  print $ANN_MAN_CORR_FH "$to_name_base.gz: $from_name_base\n";
+  print $ANN_MAN_DESCR_FH "$to_name_base.gz: $fr_to_hsh->{description}\n";
   my $SPLICE_RX = $fr_to_hsh->{splice};
   $SPLICE_RX =~ s/\"//g; # Strip surrounding quotes if any. We'll add them below.
   my $STRIP_RX = $fr_to_hsh->{strip};
@@ -214,7 +254,6 @@ for my $fr_to_hsh (@{$confobj->{from_to_protein}}){
   say "  SPLICE REGEX: \"$SPLICE_RX\"";
   say "  STRIP REGEX: \"$STRIP_RX\"";
   my $ARGS = "-hash $GENE_HASH -fasta $SEQ_FROM_FILE -splice \"$SPLICE_RX\" -strip \"$STRIP_RX\" -nodef -out $SEQ_TO_FILE";
-  #say "hash_into_fasta_id.pl $ARGS\n";
   system("hash_into_fasta_id.pl $ARGS");
 }
 
@@ -226,12 +265,31 @@ for my $fr_to_hsh (@{$confobj->{from_to_gff}}){
   say "Converting from to:";
   say "  $GFF_FROM_FILE";
   say "  $GFF_TO_FILE";
+  my $to_name_base = basename($GFF_TO_FILE);
+  my $from_name_base = basename($GFF_FROM_FILE);
+  print $ANN_MAN_CORR_FH "$to_name_base.gz: $from_name_base\n";
+  print $ANN_MAN_DESCR_FH "$to_name_base.gz: $fr_to_hsh->{description}\n";
   my $STRIP_RX = $fr_to_hsh->{strip};
   $STRIP_RX =~ s/\"//g; # Strip surrounding quotes if any. We'll add them below.
   say "  STRIP REGEX: \"$STRIP_RX\"";
   my $ARGS = "-gff $GFF_FROM_FILE -gene_hash $GENE_HASH -seqid_hash $CHR_HASH -strip \"$STRIP_RX\" -sort -out $GFF_TO_FILE";
-  #say "hash_into_fasta_id.pl $ARGS\n";
   system("hash_into_gff_id.pl $ARGS");
+}
+
+##################################################
+say "\n== Processing the genome assembly files ==";
+for my $fr_to_hsh (@{$confobj->{from_to_genome}}){ 
+  my $GNM_FROM_FILE = "$WD/$dir_hsh{from_genome_dir}/$prefix_hsh{from_genome_prefix}.$fr_to_hsh->{from}";
+  my $GNM_TO_FILE = "$GNMDIR/$GENSP.$GNMCOL.$fr_to_hsh->{to}";
+  say "Converting from to:";
+  say "  $GNM_FROM_FILE";
+  say "  $GNM_TO_FILE";
+  my $to_name_base = basename($GNM_TO_FILE);
+  my $from_name_base = basename($GNM_FROM_FILE);
+  print $GNM_MAN_CORR_FH "$to_name_base.gz: $from_name_base\n";
+  print $GNM_MAN_DESCR_FH "$to_name_base.gz: $fr_to_hsh->{description}\n";
+  my $ARGS = "-hash $CHR_HASH -fasta $GNM_FROM_FILE -nodef -out $GNM_TO_FILE";
+  system("hash_into_fasta_id.pl $ARGS");
 }
 
 __END__
