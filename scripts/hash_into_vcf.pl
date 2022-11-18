5f3834ce29ee4f2a58b751829441e9cf0c8b1c39
Steven Cannon <scannon@iastate.edu>
2022-11-18 15:49:45 -0600

    Add option to construct and add IDs to VCF

--- scripts/hash_into_vcf.pl
+++ scripts/hash_into_vcf.pl
@@ -8,20 +8,22 @@ my $usage = <<EOS;
         OR     hash_into_vcf.pl -hash FILE < FILE.vcf
   
   Read a key-value hash file, and VCF data on STDIN.
-  Swap the IDs with the values from the hash file.
+  Swap the CHROM_IDs with the values from the hash file.
 
   Required:
-  -hash      (string) Key-value hash file, where first column has IDs from VCF file
+  -hash      (string) Key-value hash file, where first column has CHROM_IDs from VCF file
   
   Options:
+  -add_id (boolean) Construct and write a feature ID in the third column, with the form CHROM.POS
   -help   (boolean) This message.
   
 EOS
 
-my ($hash_file, $help);
+my ($hash_file, $add_id, $help);
 
 GetOptions (
   "hash_file=s" =>  \$hash_file,   # required
+  "add_id" =>       \$add_id,
   "help" =>         \$help,
 );
 
@@ -35,21 +37,50 @@ while (<$HSH>) {
   chomp;
   /(\S+)\s+(.+)/;
   next if (/^#/);
-  my ($id, $hash_val) = ($1,$2);
-  $id_map{$id} = $hash_val;   # print "$id, $id_map{$id}\n";
+  my ($chr_id, $hash_val) = ($1,$2);
+  $id_map{$chr_id} = $hash_val;   # print "$chr_id, $id_map{$chr_id}\n";
 }
 
 # read VCF data file on STDIN and swap IDs
+my $comment_str = "";
+my ($gene_name, $new_id);
 while (<>) {
   chomp;
-  my $line = $_;
-  if ($_ =~ /^#/){
-    print "$line\n";
-    next;
+
+  # print comment line 
+  if (/(^#.+)/) {
+    $comment_str=$1;
+    if($comment_str=~/##contig=<ID=([^,]+),length=(\d+)>/){
+      if ($id_map{$1}){
+        my $new_chr_ID=$id_map{$1};
+        print "##contig=<ID=$new_chr_ID,length=$2>\n";
+      }
+    }
+    else { # structure like "##contig=<ID=CM003504.1,length=36501346>" isn't seen, so print what was there
+      print "$comment_str\n";
+    }
   }
-  my ($first, @rest) = split(/\t/, $line);
-  my $new_id = $id_map{$first}; 
-  die "could not find $new_id\n" unless defined $new_id;
-  print "$new_id\t", join("\t", @rest), "\n";
+  else { # body of the VCF
+    my @fields = split(/\t/, $_);
+    my $chr_id = shift(@fields);
+    my $pos = shift(@fields);
+    my $current_id = shift(@fields);
+    if ($id_map{$chr_id}){
+      if ($add_id){
+        my $feat_id = "$id_map{$chr_id}.$pos";
+        print "$id_map{$chr_id}\t$pos\t$feat_id\t", join("\t", @fields), "\n";
+      }
+      else { # don't print constructed feat_id
+        print "$id_map{$chr_id}\t$pos\t$current_id\t", join("\t", @fields), "\n";
+      }
+    }
+    else {
+      warn "Warning: No matching ID for contig $chr_id\n";
+    }
+  }
 }
 
+__END__
+S. Cannon
+2022-11-17 New script
+2022-11-18 Handle identifiers in vcf comment block, e.g. ##contig=<ID=CM003504.1,length=36501346>
