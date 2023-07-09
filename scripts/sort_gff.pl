e3380a5490c9d397800637823e7f4d0c8bd016f9
Steven Cannon <scannon@iastate.edu>
2023-07-09 11:22:10 -0500

    Add some types for collate sort. With extra items, switch to floats (0.00..0.23) for the collate sort.

--- scripts/sort_gff.pl
+++ scripts/sort_gff.pl
@@ -4,17 +4,30 @@ use strict;
 # PROBLEM (SCannon note): the second sort item ([3]) is genomic position, but some genes are overlapping.
 
 my %type_collate = (
-    gene => 0,
-    mRNA => 1,
-    ncRNA => 1.5,
-    rRNA => 1.75,
-    tRNA => 1.875,
-    exon => 2,
-    three_prime_UTR => 3,
-    CDS => 4,
-    five_prime_UTR => 5,
-    protein_match => 6,
-    match_part => 7,
+  region => 0.00,
+  gene => 0.01,
+  pseudogene => 0.02,
+  mRNA => 0.03,
+  transcript => 0.04,
+  ncRNA => 0.05,
+  lnc_RNA => 0.06,
+  snoRNA => 0.07,
+  snRNA => 0.08,
+  rRNA => 0.09,
+  tRNA => 0.10,
+  exon => 0.11,
+  three_prime_UTR => 0.12,
+  CDS => 0.13,
+  five_prime_UTR => 0.14,
+  protein_match => 0.15,
+  cDNA_match => 0.16,
+  match => 0.17,
+  match_part => 0.18,
+  protein_match => 0.19,
+  expressed_sequence_match => 0.20,
+  translated_nucleotide_match => 0.21,
+  expressed_sequence_match => 0.22,
+  contig => 0.23,
 );
 my $line;
 while ($line = <>) {
@@ -29,3 +42,6 @@ my @lines = ($line, <>);
 foreach my $line (@lines) {
     print join("\t", @$line);
 }
+
+# Changes
+#2023-07-08 Add some types for collate sort. With extra items, switch to floats (0.00..0.23) for the collate sort.
