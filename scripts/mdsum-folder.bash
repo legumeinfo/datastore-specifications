65d5f79dc63f288c8addf22255a64a612ab87977
Andrew Farmer <adf@ncgr.org>
2023-11-22 14:12:16 -0600

    busybox md5sum no longer needed after BSD upgrade, per nathanweeks

--- scripts/mdsum-folder.bash
+++ scripts/mdsum-folder.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [[ $# -ne 1 || ! -d $1 ]]; then 
     echo USAGE: $0 /path/to/datastore/folder
     exit 1
@@ -11,7 +11,5 @@ collection=`basename $path`
 md5_name=CHECKSUM.$collection.md5
 
 if [[ -e $md5_name ]]; then rm $md5_name; fi
-#FIXME: this will work on LIS using BSD. if anyone else ever uses it, will need to test OS and adjust accordingly I think
-#find . -type f -not -name $md5_name | xargs md5 -r > $md5_name
-find . -type f -not -name $md5_name -not -name ".nfs*" | xargs busybox md5sum > $md5_name
+find . -type f -not -name $md5_name -not -name ".nfs*" | xargs md5sum > $md5_name
 popd
