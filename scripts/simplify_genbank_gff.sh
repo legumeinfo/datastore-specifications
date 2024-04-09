9edb9a2b47b65b8d8ccc92af15f42eaf0e4a6c08
Steven Cannon <scannon@iastate.edu>
2024-04-08 20:18:03 -0500

    Handle rRNA records

--- scripts/simplify_genbank_gff.sh
+++ scripts/simplify_genbank_gff.sh
@@ -19,11 +19,11 @@ fi
 
 perl -pe 's/\|WGS:\w+//g; s/\w+-gnl\|//g; s/;(Note=|gbkey=|Dbxref=|Name=).+//' | # Simplify 9th col to ID or ID+Parent
   perl -pe 's/rna-//g; s/gene-//g; s/exon-//g' | # Strip feature types from IDs except cds
-  perl -pe 's/ID=(cds-[^;]+);Parent=(.+)/ID=$2;Parent=$2/' |  # For CDS features, replace with name of parent
+  perl -pe 's/ID=(cds-[^;]+);Parent=(.+)/ID=$2;Parent=$2/' | # For CDS features, replace ID with name of parent
   perl -pe 's/;/\t/g' |  # Temporarily split 9th column on semicolons
-  awk -v OFS="\t" '$3 ~ /CDS/ && $9 != prev {ct=1; print $1, $2, $3, $4, $5, $6, $7, $8, $9 "-" ct, $10; prev=$9; next}
-                   $3 ~ /CDS/ && $9 == prev {ct++; print $1, $2, $3, $4, $5, $6, $7, $8, $9 "-" ct, $10; prev=$9}
-                   $3 !~ /CDS/ {print}' |
+  awk -v OFS="\t" '$3 ~ /CDS|[rt]RNA/ && $9 != prev {ct=1; print $1, $2, $3, $4, $5, $6, $7, $8, $9 "-" ct, $10; prev=$9; next}
+                   $3 ~ /CDS|[rt]RNA/ && $9 == prev {ct++; print $1, $2, $3, $4, $5, $6, $7, $8, $9 "-" ct, $10; prev=$9}
+                   $3 !~ /CDS|[rt]RNA/ {print}' |
   awk -v OFS="\t" '$3 ~ /exon/ {ID_val = substr($9,4); print $1, $2, $3, $4, $5, $6, $7, $8, "ID=exon-" ID_val, $10}
                    $3 !~ /exon/ {print}' |
   perl -pe 's/ID=(\S+)$/ID=$1;Name=$1/; s/ID=(\S+)\tParent=(\S+)/ID=$1;Parent=$2;Name=$1/'
@@ -33,3 +33,4 @@ perl -pe 's/\|WGS:\w+//g; s/\w+-gnl\|//g; s/;(Note=|gbkey=|Dbxref=|Name=).+//' |
 # Versions
 # 2023-07-05 First version. 
 # 2023-07-08 Fix bug in naming of exon features
+# 2024-04-07 Handle rRNA records
