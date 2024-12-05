12f8e9b1f92c6801ca095e2eef231ea6b0693d2d
Steven Cannon <scannon@iastate.edu>
2024-12-05 11:41:21 -0600

    Handle additional ninth-column cases

--- scripts/add_name_attr_to_gff.pl
+++ scripts/add_name_attr_to_gff.pl
@@ -16,20 +16,30 @@ Usage: zcat FILE.gff3.gz | perl $0
 EOS
 
 while (<>){
+  chomp;
   my $line = $_;
-  if ($line =~ /^#/){ print $line }
+  if ($line =~ /^#/){ print "$line\n" }
   else {
     my @parts = split(/\t/, $line);
     if ($parts[2] =~ /gene|mRNA/ && $parts[8] !~ /Name=/){
       my @first_eight = @parts[0..7];
       my $ninth = $parts[8];
-      $ninth =~ s/ID=([^.]+\.[^.]+\.[^.]+\.[^.]+)\.([^;]+);(.+)/ID=$1.$2;Name=$2;$3/; 
-      print join("\t", @first_eight, $ninth);
+      if ( $ninth =~ /ID=[^;]+;.+/ ){ # There are multiple attributes, starting with ID
+        $ninth =~ s/ID=([^.]+\.[^.]+\.[^.]+\.[^.]+)\.([^;]+);(.+)/ID=$1.$2;Name=$2;$3/; 
+      }
+      elsif ( $ninth =~ /ID=[^;]+$/ ) { # There is a single attribute (just ID)
+        $ninth =~ s/ID=([^.]+\.[^.]+\.[^.]+\.[^.]+)\.([^;]+)$/ID=$1.$2;Name=$2/; 
+      }
+      else { die "Unexpected structure in ninth column: \n  [$ninth]\n" }
+      print join("\t", @first_eight, $ninth), "\n";
     }
     else {
-      print $line;
+      print "$line\n";
     }
   }
 }
 
+__END__
 
+2024-11-25 sbc Initial version
+2024-11-26 sbc Handle additional ninth-column cases
